c2b300c2b7d4f4a199e5c36513fdec05221b6609
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-10-23 00:01:10 +0600

    news show complete

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCard from '../Shared/NewsSummaryCard/NewsSummaryCard';
 
 const Home = () => {
+    const allNews = useLoaderData();
     return (
         <div>
-            <h3>this is home</h3>
+            <h3>News today:{allNews.length}</h3>
+            {
+                allNews.map( news => <NewsSummaryCard
+                key={news._id}
+                news={news}
+                ></NewsSummaryCard>)
+            }
         </div>
     );
 };
