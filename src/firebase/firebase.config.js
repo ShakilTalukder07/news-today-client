4d64fa25428178452fb990ff1626ee0dcd5375ad
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-10-22 19:12:10 +0600

    data loaded

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_rujeGx1VUhoND_RqPkSMVQqA8s4rNts",
+  authDomain: "news-today-4f23d.firebaseapp.com",
+  projectId: "news-today-4f23d",
+  storageBucket: "news-today-4f23d.appspot.com",
+  messagingSenderId: "374575616109",
+  appId: "1:374575616109:web:15ffd3948205cfc554ef64"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
